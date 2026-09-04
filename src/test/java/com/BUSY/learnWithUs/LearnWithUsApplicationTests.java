be7adf43c6f49c83518b3d24cbed1a0213d77f59
Rahul Bhatt <rbhatt.cs@gmail.com>
2026-09-04 22:01:53 +0530

    initializing project and adding dependancies

---
+++ src/test/java/com/BUSY/learnWithUs/LearnWithUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BUSY.learnWithUs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWithUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
